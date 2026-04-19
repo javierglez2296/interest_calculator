cd7b3743df1d432d8a20d278f5eb49f86b2a3ea1
javierglez2296 <81320903+javierglez2296@users.noreply.github.com>
2026-04-19 13:37:49 +0200

    Update supabase_client.py

--- supabase_client.py
+++ supabase_client.py
@@ -5,7 +5,6 @@ from utils.config import SUPABASE_URL, SUPABASE_SERVICE_ROLE_KEY
 def get_supabase_admin():
     if not SUPABASE_URL:
         raise ValueError("SUPABASE_URL no configurada")
-
     if not SUPABASE_SERVICE_ROLE_KEY:
         raise ValueError("SUPABASE_SERVICE_ROLE_KEY no configurada")
 
