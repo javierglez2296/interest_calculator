32cf64ba71fcd59d073c50f8dffb7535c1d878e7
javierglez2296 <81320903+javierglez2296@users.noreply.github.com>
2026-04-26 17:11:50 +0200

    Create supabase_client.py

---
+++ utils/supabase_client.py
@@ -0,0 +1,11 @@
+from supabase import create_client
+from utils.config import SUPABASE_URL, SUPABASE_SERVICE_ROLE_KEY
+
+
+def get_supabase_admin():
+    if not SUPABASE_URL:
+        raise ValueError("SUPABASE_URL no configurada")
+    if not SUPABASE_SERVICE_ROLE_KEY:
+        raise ValueError("SUPABASE_SERVICE_ROLE_KEY no configurada")
+
+    return create_client(SUPABASE_URL, SUPABASE_SERVICE_ROLE_KEY)
